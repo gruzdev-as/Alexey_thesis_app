0e910df44c4a9e91423c009aa2c36711f569823d
gruzdev-as <gruzdev-as@yandex.ru>
2024-03-23 22:53:02 +0800

    Added search function.

--- application.py
+++ application.py
@@ -33,18 +33,20 @@ class MainApplication(QtWidgets.QMainWindow, design.Ui_MainWindow):
             )
         self.second_display_widget_lay.addWidget(self.graph_second_widget) 
         ## Constants
-        self.penetrability = 3e-11
-        self.viscosity = 3e-4
+        self.penetrability = 1.5e-10
+        self.viscosity = 0.3
         self.density =  1.1e3
         self.preassure_start = 101325
         self.preassure_end = 15199
-        self.capnum = 2.5e-4
+        self.capnum = 1000
         self.sur_tension = 4.6e-2
+        self.v_cap = 0.0025
         ## Events 
         self.choosedata_button.clicked.connect(self.choose_data_file)
         self.build_poly_button.clicked.connect(self.draw_polynom)
-        self.calculate_length_button.clicked.connect(self.calculate_length)
+        self.calculate_length_button.clicked.connect(self.show_lentgh)
         self.adjust_coef_button.clicked.connect(self.show_additional_window)
+        self.start_search_button.clicked.connect(self.binary_search_points)
     
     def choose_data_file(self):
         
@@ -55,13 +57,15 @@ class MainApplication(QtWidgets.QMainWindow, design.Ui_MainWindow):
         if file_path:
             with open(file_path, 'r') as f:
                 data_df = pd.read_csv(file_path, sep=';', decimal='.')
-                self.X_data, self.Y_data = data_df.X.to_numpy(), data_df.Y.to_numpy()
+                self.X_data, self.Y_data = data_df.X.to_numpy() / 1000, data_df.Y.to_numpy() / 1000
         else:
             return
 
         #Draw Point in the graph
         self.graph_first_widget.axes.plot(self.X_data, self.Y_data, '.')
         self.graph_first_widget.draw()
+        self.build_poly_button.setEnabled(True)
+        self.poly_degree_spinBox.setEnabled(True)
         
     def draw_polynom(self):
         
@@ -71,7 +75,9 @@ class MainApplication(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.x_poly_values = self.X_data
         self.y_poly_values = self.polynom(self.X_data)
         
-        # Draw the line 
+        # Draw the line and points 
+        self.graph_first_widget.axes.clear()
+        self.graph_first_widget.axes.plot(self.X_data, self.Y_data, '.')
         self.graph_first_widget.axes.plot(self.x_poly_values, self.y_poly_values, color='red')
         self.graph_first_widget.draw()
 
@@ -80,11 +86,27 @@ class MainApplication(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.formula = sum(S("{:}".format(v))*x**i for i, v in enumerate(coef[::-1]))
         self.poly_display_textbrowser.setText(str(self.formula))
 
-    def calculate_length(self):
+        self.pointA_lineEdit.setEnabled(True)
+        self.pointB_lineedit.setEnabled(True)
+        self.calculate_length_button.setEnabled(True)
+        self.adjust_coef_button.setEnabled(True)
+        self.start_search_button.setEnabled(True)
 
+    def show_lentgh(self):
+        
         a = int(self.pointA_lineEdit.text())
         b = int(self.pointB_lineedit.text())
-        
+
+        length = self.calculate_length(a, b)
+
+        self.length_label.setText(f'Длина участка от А до B = {length}')
+
+        # Draw the trajectory and points on it in the second view
+        self.graph_first_widget.axes.plot(a, self.polynom(a), '.', markersize=10., color='red')
+        self.graph_first_widget.axes.plot(b, self.polynom(b), '.', markersize=10., color='red')
+        self.graph_first_widget.draw()
+
+    def calculate_length(self, a, b):
         def calc_derr(x_):
 
             x = symbols('x')
@@ -93,13 +115,7 @@ class MainApplication(QtWidgets.QMainWindow, design.Ui_MainWindow):
         
         length, _ = quad(lambda x: math.sqrt(1+(calc_derr(x)**2)), a, b)
         
-        self.length_label.setText(f'Длина участка от А до B = {length}')
-
-        # Draw the trajectory and points on it in the second view
-        self.graph_second_widget.axes.plot(self.x_poly_values, self.y_poly_values, color='black')
-        self.graph_second_widget.axes.plot(a, self.polynom(a), '.', markersize=10., color='red')
-        self.graph_second_widget.axes.plot(b, self.polynom(b), '.', markersize=10., color='red')
-        self.graph_second_widget.draw()
+        return length
 
     def show_additional_window(self):
         '''Show the window with additional hydro_system settigs'''
@@ -127,3 +143,61 @@ class MainApplication(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.edit_window.setAttribute(QtCore.Qt.WidgetAttribute.WA_DeleteOnClose, True)   
         self.edit_window.submitClicked.connect(aplly_changes)
         self.edit_window.show()
+    
+    def binary_search_points(self):
+        '''Bin search for points in the trajectory'''
+
+        def calc_velocity(l0:float, h0:float, l1:float, h1:float) -> float:
+
+            return - (self.penetrability / self.viscosity) * (((self.preassure_end - self.preassure_start) / (l1 - l0)) - (self.density * 9.81 *(h1 - h0)))
+        
+        def search(left, l0, h0):
+
+            right = max(self.X_data)
+            max_l = self.calculate_length(0, right)
+
+            while left <= right:
+                
+                # Выбираем точку на прямой 
+                mid_point = (left + right) / 2
+
+                # Определяем параметры в этой точке
+                l_mid_point = self.calculate_length(0, mid_point)
+                h_mid_point = self.polynom(mid_point)
+                vf = calc_velocity(l0, h0, l_mid_point, h_mid_point)
+                if round(l_mid_point, 3) == round(max_l, 3):
+                    print('Дошел до конца прямой')
+                    return(mid_point, h_mid_point, vf, l_mid_point)
+
+                if round(vf, 4) == round(self.v_cap, 4):
+                    print(f'Получено равенство скоростей Vk = {round(self.v_cap, 4)}, Vf = {round(vf, 4)}')
+                    print(f'Получил нужное соотношение на точке {mid_point, h_mid_point}, на длине {l_mid_point}')
+                    print('#'*50)
+                    return(mid_point, h_mid_point, vf, l_mid_point)
+                elif round(vf, 4) > round(self.v_cap, 4):
+                    left = mid_point - 0.0002
+                else:
+                    right = mid_point + 0.0002
+            
+            return 0, 0, 0, 0
+
+        h0, l0 = 0, 0
+        points = []
+        left = 0
+
+        while round(left, 3) < round(max(self.X_data), 3):
+
+            X_mid, y_mid, vf, l = search(left, l0, h0)
+            points.append((X_mid, y_mid))
+            l0 = l
+            h0 = y_mid
+            left = X_mid
+        
+        for point in points:
+            self.search_textBrowser.append(str(point))
+        
+        self.graph_second_widget.axes.plot(self.x_poly_values, self.y_poly_values, color='red')
+        for point in points:
+            self.graph_second_widget.axes.plot(point[0], point[1], '.', markersize = 15, color='Black')
+        self.graph_second_widget.axes.grid()
+        self.graph_second_widget.draw()
